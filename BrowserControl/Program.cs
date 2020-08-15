bdd42ba89f4076daa7c4b997a25c55cae4b16ef8
ninjafiveo <mjsekol@live.com>
2020-08-15 00:18:36 -0400

    Still Just A Test

--- BrowserControl/Program.cs
+++ BrowserControl/Program.cs
@@ -2,6 +2,8 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+//^^ when grabbing the NUGET package selenium, you also need to install the chrome drivers from the selenium package. 
+
 
 namespace BrowserControl
 {
@@ -13,16 +15,23 @@ namespace BrowserControl
             // in the terminal run: dotnet run
             //"dotnet run" will open the chrome browswer and run the program. 
             IWebDriver driver = new ChromeDriver(@"C:\Users\Sekol_Michael\source\repos\BrowserControl\BrowserControl\bin\Debug\netcoreapp3.1");
-            //IWebDriver ffDriver = new FirefoxDriver(@"C:\Users\Sekol_Michael\source\repos\BrowserControl\BrowserControl\bin\Debug\netcoreapp3.1");
+            IWebDriver ffDriver = new FirefoxDriver(@"C:\Users\Sekol_Michael\source\repos\BrowserControl\BrowserControl\bin\Debug\netcoreapp3.1");
             driver.Url = "https://www.google.com";
+            ffDriver.Url = "https://www.bing.com";
 
             // Console.WriteLine(driver.Title);
             IWebElement textbox = driver.FindElement(By.Name("q"));
+            IWebElement fftextbox = ffDriver.FindElement(By.Name("q"));
             textbox.SendKeys("Bushido");
+            fftextbox.SendKeys("Ninja Turtles");
             textbox.Submit();
+            fftextbox.Submit();
             // Console.WriteLine(driver.Title);
 
             // driver.Quit(); Closes the browser
+            Console.WriteLine("Just checking the version control...");
+            Console.ReadLine();
+
         }
     }
 }
